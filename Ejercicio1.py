03eff898a194f09af0c949c35b2008f34dc88876
Daniel Buaon <dbuaon@gmail.com>
2022-05-06 20:21:11 -0300

    ej 2

---
+++ Ejercicio1.py
@@ -0,0 +1,23 @@
+import random
+import threading
+import time
+import logging
+
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+def imprimir():
+    logging.info(f'Arrancó {threading.current_thread().name}, somos {threading.active_count()} threads' )
+    time.sleep(random.randint(1,5))
+    logging.info(f'Terminó {threading.current_thread().name}, somos {threading.active_count()} threads')
+
+if __name__ == '__main__':
+    threads = []
+    for i in range(10):
+        threads.append(threading.Thread(target=imprimir))
+
+    for i in range(10):
+        threads[i].start()
+
+    for i in range(10):
+        threads[i].join()
+
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Ejercicios:
 
 1. Implemente un programa que ejecute 10 hilos que impriman un mensaje identificando al hilo, luego esperen un tiempo aleatorio entre 1 y 5 segundos y luego impriman un mensaje indicando que terminaron (identificando al hilo)
+2. Modifique el programa anterior de modo que pueda medir e imprimir el tiempo total que tomo ejecutarse cada hilo (en milisengundos)
 
 
 
